0181411f7d7afcd4b96259bf774addd4c7bcb48f
Parker Boeing <parker.boeing@gmail.com>
2023-04-11 14:26:08 -0600

    Add puts statements with modified variables

--- variables.rb
+++ variables.rb
@@ -1,8 +1,12 @@
-name = Parker
-favorite_food = Ethiopian
-favorite_book = Paradise Lost
+user_name = "Parker"
+favorite_food = "Ethiopian"
+favorite_book = "Paradise Lost"
 age = 31
 number_of_pets = 3
 climbing_grade = 5.13
 awake = true
 short_hair = false
+user_name = "Don Luigi"
+favorite_food = "Thai"
+puts ("Hi, my name is #{user_name}.") 
+puts ("my favorite food is #{favorite_food} and I am #{age} years old.")
